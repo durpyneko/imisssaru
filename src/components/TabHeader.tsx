00a526772572a197ece1e265cf196161b77b9338
durpyneko <89787577+iDurpyDude12@users.noreply.github.com>
2023-09-19 16:38:36 +0200

    update favicon
    updated favicon to be saved server side instead of a url

--- src/components/TabHeader.tsx
+++ src/components/TabHeader.tsx
@@ -9,11 +9,7 @@ export default function TabHeader({ title }: TabHeaderProps) {
   return (
     <Helmet>
       <title>{title}</title>
-      <link
-        rel="icon"
-        type="image/png"
-        href="https://cdn.discordapp.com/emojis/1101698612116852746.webp?size=32"
-      />
+      <link rel="icon" type="image/png" href="sarueiBlush.webp" />
     </Helmet>
   );
 }
